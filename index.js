3e053e8a013ff472d7f6d3f423dc83baed7ac8a0
John Brunelle <Johnzors@gmail.com>
2016-10-15 15:43:51 -0400

    Fixed token issue

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+    res.send('1902432201')
 })
 
 // for Facebook verification
