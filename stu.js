9e3819662d1aa3b924d77ecb57044a6d0ecffcd5
Stuart-Wilcox <stuart_wilcox@outlook.com>
2016-10-16 00:43:55 -0400

    1st Attempt at Image Messaging

--- stu.js
+++ stu.js
@@ -4,13 +4,33 @@
 // These functions CAN be called within index.js
 module.exports = {
   foo: function () {
-    // something else
+    return "connected"
   },
-  bar: function () {
-    // whatever
+  sendImage: function (sender, image) {
+    sendImageMessage(sender, image);
   }
 };
 
 // THESE CANNOT, BUT LOGIC CAN BE DONE HERE
 var zemba = function () {
+}
+
+function sendImageMessage(sender, text){
+  let messageData={text:text};
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body){
+    if(error){
+      console.log('Error sending messages: ', error);
+    }
+    else if(response.body.error){
+      console.log('Error: ', response.body.error);
+    }
+  })
 }
