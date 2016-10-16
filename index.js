e2772ea1e4a288e9170ab852b40e7665be2ec720
John Brunelle <Johnzors@gmail.com>
2016-10-15 23:16:18 -0400

    Unscrewed up the bot lol

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
+/**
+ * Created by john on 2016-10-15.
+ */
 
+'use strict'
 
-'use strict'//Do it this way ONLY
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-const app = express();
-
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000))
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}))
 
 // Process application/json
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
     res.send('1902432201')
-});
+})
 
 // for Facebook verification
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging;
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i];
-        let sender = event.sender.id;
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            let result = answerMathQuestion(text);
-             sendTextMessage(sender, "= " + result)
-        
-        }
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
     }
-    res.sendStatus(200)
-});
+    res.send('1323341855')
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
-});
+})
 
-const token = process.env.PAGE_ACCESS_TOKEN;
+const token = process.env.PAGE_ACCESS_TOKEN
 
 function sendTextMessage(sender, text) {
-
-    let messageData = { text:text };
+    let messageData = { text:text }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
@@ -61,13 +54,4 @@ function sendTextMessage(sender, text) {
             console.log('Error: ', response.body.error)
         }
     })
-}
-
-function Stu(){
-    let a="Hello, World!";
-    return a;
-}
-function answerMathQuestion(text) {
-    var x=eval(text);
-    return x;
 }
