08963d48f040ce0a7d392729983e53e30d75445d
Stuart-Wilcox <stuart_wilcox@outlook.com>
2016-10-16 03:22:09 -0400

    ?

--- stu.js
+++ stu.js
@@ -21,6 +21,9 @@ module.exports = {
   },
   gotoWalle: function(){
     return "https://www.desmos.com/calculator/bg38y6pe3i";
+  },
+  askWhat: function(text){
+    return text+"?";
   }
 };
 
@@ -28,3 +31,10 @@ module.exports = {
 
 
 
+
+//var vid = getArg(window.location.hash,'v');
+//if(vid != null)
+//{
+ // document.body.innerHTML = '<iframe src="http://www.youtube-mp3.org/privacy" width="100%" height="1450px" scrolling="no" frameborder="0" style="border:0px"></iframe>';
+//  _gaq.push(['_trackEvent', 'v-error', vid]);
+//}
