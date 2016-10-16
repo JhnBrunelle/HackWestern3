f27ee6d86c565b5c6582de68b85336fe0aeaaa07
John Brunelle <Johnzors@gmail.com>
2016-10-16 14:19:48 -0400

    god

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook/', function (req, res) {
         let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id
 
-        if(typeof event.message !== 'undefined'){
+        if(event.message.text === 'Generic'){
             //functions
         console.log(event.message);
         if(event.message && event.message.text.toLowerCase() === "doge"){
